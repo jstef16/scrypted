65dff34ed891054b60bfbcf76d59c4257a785308
Jordan Steffan <jordansteffan@Jordans-Mini.localdomain>
2024-06-23 15:44:09 -0500

    initial attempt

--- plugins/homekit/src/types/windowcovering.ts
+++ plugins/homekit/src/types/windowcovering.ts
@@ -1,4 +1,4 @@
-import { Entry, EntrySensor, ScryptedDevice, ScryptedDeviceType, ScryptedInterface } from '@scrypted/sdk';
+import { Brightness, Entry, EntrySensor, ScryptedDevice, ScryptedDeviceType, ScryptedInterface } from '@scrypted/sdk';
 import { addSupportedType, bindCharacteristic, DummyDevice, } from '../common';
 import { Characteristic, CharacteristicEventTypes, CharacteristicSetCallback, CharacteristicValue, NodeCallback, Service } from '../hap';
 import { makeAccessory } from './common';
@@ -9,37 +9,58 @@ addSupportedType({
     probe(device: DummyDevice): boolean {
         return device.interfaces.includes(ScryptedInterface.Entry) && device.interfaces.includes(ScryptedInterface.EntrySensor);
     },
-    getAccessory: async (device: ScryptedDevice & Entry & EntrySensor, homekitPlugin: HomeKitPlugin) => {
+    getAccessory: async (device: ScryptedDevice & Entry & EntrySensor & Brightness, homekitPlugin: HomeKitPlugin) => {
         const accessory = makeAccessory(device, homekitPlugin);
 
         const service = accessory.addService(Service.WindowCovering, device.name);
 
-        bindCharacteristic(device, ScryptedInterface.EntrySensor, service, Characteristic.CurrentPosition,
-            () => !!device.entryOpen ? 100 : 0);
+        if (device.interfaces.includes(ScryptedInterface.Brightness)) {
 
-        bindCharacteristic(device, ScryptedInterface.EntrySensor, service, Characteristic.TargetPosition,
-            () => !!device.entryOpen ? 100 : 0);
+            let props = {
+                minValue: 0,
+                maxValue: 100,
+                minStep: 1,
+            };
 
-        let props = {
-            minValue: 0,
-            maxValue: 100,
-            minStep: 100,
-        };
-        let targetState = !!device.entryOpen ? 100 : 0;
-        service.getCharacteristic(Characteristic.TargetPosition)
-            .setProps(props)
-            .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
-                callback();
-                if (value === 100) {
-                    targetState = 100;
-                    device.openEntry();
-                }
-                else {
-                    targetState = 0;
-                    device.closeEntry();
-                }
-            })
+            bindCharacteristic(device, ScryptedInterface.EntrySensor, service, Characteristic.CurrentPosition,
+                () => device.brightness);
 
+            service.getCharacteristic(Characteristic.TargetPosition)
+                .setProps(props)
+                .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
+                    callback();
+                    device.setBrightness(value as number);
+                })
+
+            // bindCharacteristic(device, ScryptedInterface.Brightness, service, Characteristic.CurrentPosition, () => Math.min(Math.max(device.brightness || 0, 0), 100));
+        } else {
+
+            bindCharacteristic(device, ScryptedInterface.EntrySensor, service, Characteristic.CurrentPosition,
+                () => !!device.entryOpen ? 100 : 0);
+
+            bindCharacteristic(device, ScryptedInterface.EntrySensor, service, Characteristic.TargetPosition,
+                () => !!device.entryOpen ? 100 : 0);
+
+            let props = {
+                minValue: 0,
+                maxValue: 100,
+                minStep: 100,
+            };
+            let targetState = !!device.entryOpen ? 100 : 0;
+            service.getCharacteristic(Characteristic.TargetPosition)
+                .setProps(props)
+                .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
+                    callback();
+                    if (value === 100) {
+                        targetState = 100;
+                        device.openEntry();
+                    }
+                    else {
+                        targetState = 0;
+                        device.closeEntry();
+                    }
+                })
+        }
         return accessory;
     }
 });
